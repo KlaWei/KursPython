0d3d56496038f8d65748be501d025e0bdaddf10b
KlaWei <32963455+KlaWei@users.noreply.github.com>
2017-11-09 16:45:14 +0100

    updated zadanie4

--- zad4l4.py
+++ zad4l4.py
@@ -1,34 +1,6 @@
 from collections import Counter
 
 
-class Words:
-    def __init__(self, file):
-        self.file = file
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-
-        beginning = ""
-
-        for line in self.file.readlines():
-            line = line.strip()
-            words = line.split()
-            if words:
-                words[0] = beginning + words[0]
-                if line[-1:] == '-':
-                    beginning = words[-1][:-1]
-                    del words[-1]
-                else:
-                    beginning = ""
-                for word in words:
-                    word = word.strip(",.;“”:’?!\"()")
-                    if word != "":
-                        return word
-        StopIteration()
-
-
 def words(file):
     beginning = ""
 
@@ -64,8 +36,3 @@ if __name__ == "__main__":
         for word in words(input_file):
             print(word)
     print(stats(filename))
-
-    with open(filename) as input_file:
-        print('iterator:')
-        for w in Words(input_file):
-            print(str(w))
